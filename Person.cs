d1e4da48123e490b750be038311decc83fb5867f
McDonnex Thyolera Kamsonga <mmconnexkamsonnga@gmail.com>
2023-12-24 10:06:41 +0200

    Person class has been added

---
+++ Person.cs
@@ -0,0 +1,8 @@
+// Person.cs
+public class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    public string UniqueID { get; set; }
+}
